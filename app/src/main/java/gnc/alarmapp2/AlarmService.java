1caca37b8a413ce47db22a7b9e66e2b6407f7a6f
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2018-09-16 15:54:41 +0600

    notification works with ringtone

---
+++ app/src/main/java/gnc/alarmapp2/AlarmService.java
@@ -0,0 +1,44 @@
+package gnc.alarmapp2;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class AlarmService extends IntentService {
+    private NotificationManager alarmNotificationManager;
+
+    public AlarmService() {
+        super("AlarmService");
+    }
+
+    @Override
+    public void onHandleIntent(Intent intent) {
+        sendNotification("Wake Up! Wake Up!");
+    }
+
+    private void sendNotification(String msg) {
+        NotificationHandler.showNotificationWithoutPendingIntent(this,
+                new NotificationHandler.NotificationMessage("demo title", msg),R.mipmap.ic_launcher_round);
+        Log.d("AlarmService", "Preparing to send notification...: " + msg);
+//        alarmNotificationManager = (NotificationManager) this
+//                .getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+//                new Intent(this, MainActivity.class), 0);
+//
+//        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+//                this).setContentTitle("Alarm").setSmallIcon(R.mipmap.ic_launcher)
+//                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+//                .setContentText(msg);
+//
+//
+//        alamNotificationBuilder.setContentIntent(contentIntent);
+//        alarmNotificationManager.notify(1, alamNotificationBuilder.build());
+//        Log.d("AlarmService", "Notification sent.");
+    }
+}
+
---
+++ app/src/main/java/gnc/alarmapp2/NotificationHandler.java
@@ -0,0 +1,147 @@
+package gnc.alarmapp2;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NotificationHandler {
+
+    public static class NotificationMessage{
+        String body;
+        String title;
+        public NotificationMessage(String title, String body){
+            this.title = title;
+            this.body = body;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body=body;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title=title;
+        }
+    }
+
+    private final static AtomicInteger c = new AtomicInteger(0);
+    private final static String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+
+    /**
+     * Shows a single notification without pending intent
+     * @param context
+     * @param remoteMessage
+     * @param notificationIconResId
+     */
+    public static void showNotificationWithoutPendingIntent(Context context, final NotificationMessage remoteMessage, int notificationIconResId) {
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), notificationIconResId);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(notificationIconResId)
+                .setLargeIcon(largeIcon)
+                .setTicker(remoteMessage.getBody())
+                .setContentTitle(remoteMessage.getTitle());
+
+        notificationManager.notify(getID(), notificationBuilder.build());
+    }
+
+
+    /**
+     * Shows a single notification with pending intent
+     * @param context
+     * @param remoteMessage
+     * @param cls
+     * @param notificationIconResId
+     */
+    public static void showNotificationWithPendingIntent(Context context, final NotificationMessage remoteMessage, Class<?> cls, int notificationIconResId) {
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+
+        Intent gcmintent = new Intent(context, cls);
+        gcmintent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        int requestID = (int) System.currentTimeMillis();
+
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, requestID,
+                gcmintent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), notificationIconResId);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(notificationIconResId)
+                .setLargeIcon(largeIcon)
+                .setTicker(remoteMessage.getBody())
+                .setContentTitle(remoteMessage.getTitle())
+                .setContentIntent(contentIntent);
+
+        notificationManager.notify(getID(), notificationBuilder.build());
+    }
+
+
+    /**
+     * Returns a unique integer id to generate notification builder
+     * @return
+     */
+    private static int getID() {
+        return c.incrementAndGet();
+    }
+
+}
